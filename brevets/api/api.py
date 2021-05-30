bb8e1bcbf7dfd6a52b7c6675f9ffe7498182770a
Lindsay Marean <lmarean@bensay.org>
2021-05-29 21:00:58 -0700

    all 3 services are drafted now but api and website not working due to overwriting endpoint function

--- brevets/api/api.py
+++ brevets/api/api.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from db_methods import Db
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,20 +11,60 @@ api = Api(app)
 # make a JSON element to return
 db = Db()
 
+""" ----- JSON ----- """
+
 
 class ListAll(Resource):
     def get(self):
-        return db.find_content()
+        return jsonify(db.find_content({open_time: 1, close_time: 1, _id: 0}))
 
 
 class ListOpenOnly(Resource):
     def get(self):
-        return db.find_content({open_time: 1, _id: 0})
+        return jsonify(db.find_content({open_time: 1, _id: 0}))
 
 
 class ListCloseOnly(Resource):
     def get(self):
-        return db.find_content({close_time: 1, _id: 0})
+        return jsonify(db.find_content({close_time: 1, _id: 0}))
+
+
+""" ----- CSV ----- """
+
+
+class ListAllCSV(Resource):
+    def get(self):
+        json_data = db.find_content(
+            jsonify({open_time: 1, close_time: 1, _id: 0}))
+        json_parsed = json.loads(json_data)
+        csv_string = ""
+        header = 0
+        for (key, value) in json_parsed.items():
+            if header == 0:
+                csv_string += json_parsed
+
+        with open('/tmp/AllTimes.csv', 'w', newline='') as fp:
+            csvwriter = csv.writer(fp)
+            count = 0
+            for time_pair in time_pairs:
+                if count == 0:
+                    header = time_pair.keys()
+                    csvwriter.writerow(header)
+                    count += 1
+                csvwriter.writerow(time_pair.values())
+        return csv_string
+
+
+class ListOpenOnlyCSV(Resource):
+    def get(self):
+        json_data = db.find_content(jsonify({open_time: 1, _id: 0}))
+        json_parsed = json.loads(json_data)
+
+
+class ListCloseOnlyCSV(Resource):
+    def get(self):
+        json_data = db.find_content(jsonify({close_time: 1, _id: 0}))
+        json_parsed = json.loads(json_data)
 
 
 # Create routes
@@ -31,6 +72,13 @@ class ListCloseOnly(Resource):
 api.add_resource(ListAll, '/listAll')
 api.add_resource(ListOpenOnly, '/listOpenOnly')
 api.add_resource(ListCloseOnly, '/listCloseOnly')
+api.add_resource(ListAll, '/listAll/json')
+api.add_resource(ListOpenOnly, '/listOpenOnly/json')
+api.add_resource(ListCloseOnly, '/listCloseOnly/json')
+
+api.add_resource(ListAllCSV, '/listAll/csv')
+api.add_resource(ListOpenOnlyCSV, '/listOpenOnly/csv')
+api.add_resource(ListCloseOnlyCSV, '/listCloseOnly/csv')
 
 
 # Run the application
---
+++ brevets/website/website.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def home():
+    render_template('display_times.html')
+
+
+@app.route('/listeverything')
+def listeverything():
+    r = requests.get('http://restapi:5000/listAll')
+    return r.text
+
+
+@app.route('/listopen')
+def listeverything():
+    r = requests.get('http://restapi:5000/listOpenOnly')
+    return r.text
+
+
+@app.route('/listclose')
+def listeverything():
+    r = requests.get('http://restapi:5000/listCloseOnly')
+    return r.text
+
+
+@app.route('/listeverythingcsv')
+def listeverything():
+    r = requests.get('http://restapi:5000/listAll/csv')
+    return r.text
+
+
+@app.route('/listopencsv')
+def listeverything():
+    r = requests.get('http://restapi:5000/listOpenOnly/csv')
+    return r.text
+
+
+@app.route('/listclosecsv')
+def listeverything():
+    r = requests.get('http://restapi:5000/listCloseOnly/csv')
+    return r.text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
